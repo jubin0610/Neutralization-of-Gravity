0a654060bf498c15c686dcbec6b512e404995814
jubin0610 <115522651+jubin0610@users.noreply.github.com>
2022-12-20 01:52:03 +0900

    마우스 클릭 시 무중력 공간으로 전환
    무중력 공간을 표현하기 위해 attractor와 mover를 활용하여 공이 떠다니는 것을 표현

--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 let ball;
+let movers = [];
+let attractor;
 
 function setup() {
   createCanvas(600, 400);
 
   ball = new Particle();
+  
+    for (let i = 0; i < 10; i++) {
+    movers[i] = new Mover(random(0.1, 2), random(width), random(height));
+  }
+  attractor = new Attractor();
 }
 
 
@@ -15,4 +22,17 @@ function draw() {
 
   ball.update();
   ball.show();
+  
+  if (mouseIsPressed) {
+    
+    attractor.display();
+
+  for (let i = 0; i < movers.length; i++) {
+    let force = attractor.calculateAttraction(movers[i]);
+    movers[i].applyForce(force);
+
+    movers[i].update();
+    movers[i].display();
+  }
+  }
 }
